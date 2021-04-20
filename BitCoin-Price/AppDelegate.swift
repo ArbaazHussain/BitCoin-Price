e0879c4023941993f55d6d317ff638593965bf3b
ArbaazHussain <akarbaaz452@gmail.com>
2021-04-20 17:16:47 +0530

    First commit.

--- BitCoin-Price/AppDelegate.swift
+++ BitCoin-Price/AppDelegate.swift
@@ -5,7 +5,12 @@
 //  Created by Arbaaz on 20/04/21.
 //
 
+
+
 import UIKit
+
+
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +22,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    
+    
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -25,12 +34,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    
+    
+    
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
+    
+    
 
 }
 
---
+++ BitCoin-Price/Model/CoinData.swift
@@ -0,0 +1,17 @@
+//
+//  CoinData.swift
+//  BitCoin-Price
+//
+//  Created by Arbaaz on 20/04/21.
+//
+
+
+
+import Foundation
+
+
+
+struct CoinData: Decodable {
+    let rate: Double
+}
+
